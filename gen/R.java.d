2d56f7b0356893263967ddc3bd4708b30ef5c174
sliversniper <sniper.sliver@gmail.com>
2013-08-31 05:28:00 +0800

    also removed the out folder

--- gen/R.java.d
+++
@@ -1,4 +0,0 @@
-C:\extracted\ZBarAndroidSDK-0\ZBarAndroidSDK-0.2\examples\CameraTest\gen\net\sourceforge\zbar\android\CameraTest\R.java \
- : C:\extracted\ZBarAndroidSDK-0\ZBarAndroidSDK-0.2\examples\CameraTest\res\layout\main.xml \
-C:\extracted\ZBarAndroidSDK-0\ZBarAndroidSDK-0.2\examples\CameraTest\res\values\strings.xml \
-C:\extracted\ZBarAndroidSDK-0\ZBarAndroidSDK-0.2\examples\CameraTest\bin\AndroidManifest.xml \
